6bc9699488093569b403d8ddc34aab9c6e17f1ad
ngocanh01 <nanh5605@gmail.com>
2024-11-23 18:48:56 +0700

    Refactor code: Move 2 functions FilterHotels and MergeHotelListWithFullData into HotelService.cs

--- HotelDataMerger/Program.cs
+++ HotelDataMerger/Program.cs
@@ -1,5 +1,4 @@
-﻿using HotelDataMerger;
-using HotelDataMerger.Models;
+﻿using HotelDataMerger.Models;
 using HotelDataMerger.Services;
 using System.Text.Json;
 
@@ -7,34 +6,29 @@ public class Program
 {
 	public static async Task Main(string[] args)
 	{
+		const string ACEME = "Acme";
+		const string PATAGONIA = "Patagonia";
+		const string PAPERFLIES = "Paperflies";
+		const string ACEME_URL = "https://5f2be0b4ffc88500167b85a0.mockapi.io/suppliers/acme";
+		const string PATAGONIA_URL = "https://5f2be0b4ffc88500167b85a0.mockapi.io/suppliers/patagonia";
+		const string PAPERFLIES_URL = "https://5f2be0b4ffc88500167b85a0.mockapi.io/suppliers/paperflies";
+
 		if (args.Length < 2)
 		{
 			Console.WriteLine("Usage: runner.bat <hotel_ids> <destination_ids>");
 			return;
 		}
 		var hotelIds = args[0] == "none" ? null : args[0].Split(',').ToList();
-		var destinationIds = args[1] == "none" ? null : args[1].Split(',').Select(int.Parse).ToList();
-
-		const string ACEME_URL = "https://5f2be0b4ffc88500167b85a0.mockapi.io/suppliers/acme";
-		const string PATAGONIA_URL = "https://5f2be0b4ffc88500167b85a0.mockapi.io/suppliers/patagonia";
-		const string PAPERFLIES_URL = "https://5f2be0b4ffc88500167b85a0.mockapi.io/suppliers/paperflies";
-
-		const string ACEME = "Acme";
-		const string PATAGONIA = "Patagonia";
-		const string PAPERFLIES = "Paperflies";
+		var destinationIds = args[1] == "none" ? null : args[1].Split(',').Select(int.Parse).ToList();		
 
 		HotelService hotelService = new();
-		DataMerger dataMerger = new();
-		HotelFilter hotelFilter = new();
-
 		List<Hotel> acemeData = await hotelService.GetHotelData(ACEME_URL, ACEME);
 		List<Hotel> patagoniaData = await hotelService.GetHotelData(PATAGONIA_URL, PATAGONIA);
 		List<Hotel> paperfliesData = await hotelService.GetHotelData(PAPERFLIES_URL, PAPERFLIES);
-
 		// merge data
-		var mergedHotels = dataMerger.MergeHotelListsWithFullData(acemeData, patagoniaData, paperfliesData);
+		var mergedHotels = hotelService.MergeHotelListsWithFullData(acemeData, patagoniaData, paperfliesData);
 		// filter data
-		var filteredHotels = hotelFilter.FilterHotels(mergedHotels, hotelIds, destinationIds);
+		var filteredHotels = hotelService.FilterHotels(mergedHotels, hotelIds, destinationIds);
 
 		var options = new JsonSerializerOptions { WriteIndented = true };
 		string jsonResult = JsonSerializer.Serialize(filteredHotels, options);
