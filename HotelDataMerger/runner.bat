6fc1866c1d53da7679c09c234b95e97a9252386f
ngocanh01 <nanh5605@gmail.com>
2024-11-25 11:27:54 +0700

    Update exe_path in runner.bat file

--- HotelDataMerger/runner.bat
+++ HotelDataMerger/runner.bat
@@ -2,4 +2,12 @@
 set hotel_ids=%1
 set destination_ids=%2
 
-"D:\Source\Repos\HotelDataMerger\HotelDataMerger\bin\Release\net8.0\win-x64\publish\HotelDataMerger.exe" %hotel_ids% %destination_ids%
+REM Get the directory of the current batch file
+set script_dir=%~dp0
+set exe_path="%script_dir%bin\Release\net8.0\win-x64\publish\HotelDataMerger.exe"
+
+REM Run the executable with the provided arguments
+%exe_path% %hotel_ids% %destination_ids%
+
+REM Pause to keep the console window open in case of errors
+pause
